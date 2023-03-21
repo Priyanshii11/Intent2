92d774e20e33373541de313fa4c53921037a21bd
Priyanshii <Priyanshik222@gmail.com>
2023-03-22 01:23:38 +0530

    finall commit

---
+++ app/src/main/java/com/priyu/intent2/MainActivity2.java
@@ -0,0 +1,36 @@
+package com.priyu.intent2;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class MainActivity2 extends AppCompatActivity {
+    Button button;
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        button = findViewById(R.id.btn2);
+        textView = findViewById(R.id.textView);
+
+        String myvalue = getIntent().getStringExtra("name");
+        textView.setText(myvalue);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity2.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
